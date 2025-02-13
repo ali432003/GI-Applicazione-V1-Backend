174f3ac609631965567974b879d168eb975fc6e8
ali432003 <alifarhaan800@gmail.com>
2025-02-14 00:23:28 +0500

    revert last commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import "dotenv/config";
 import express from "express";
-import authRouter from "../src/routes/auth.routes.js";
-import orderRouter from "../src/routes/orders.routes.js"
+import authRouter from "./src/routes/auth.routes.js";
+import orderRouter from "./src/routes/orders.routes.js"
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import swaggerDocs from "../src/lib/swagger.js";
+import swaggerDocs from "./src/lib/swagger.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
